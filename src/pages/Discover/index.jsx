efc0f10dc133eda6240c2450dc6983a7f7fa30f8
Pushpendra Singh <psnitsri01@gmail.com>
2023-07-14 15:41:26 +0530

    Adding Sidebar with responsive support

--- src/pages/Discover/index.jsx
+++ src/pages/Discover/index.jsx
@@ -1,14 +1,25 @@
+import { useState } from 'react';
+
 import Sidebar from '../../components/Sidebar';
 import Styles from './discover.module.css';
 
 function Discover() {
+  const [showMenu, toggleMenu] = useState(false);
   return (
     <div className={Styles.wrapper}>
       <aside className={Styles.sidebar}>
         <Sidebar />
       </aside>
+
+      {showMenu && (
+        <aside className={Styles.mobileMenu}>
+          <Sidebar />
+        </aside>
+      )}
+
       <main className={Styles.main}>
         <div>Search</div>
+        <button onClick={() => toggleMenu((v) => !v)}> Toggle Menu</button>
         <div className={Styles.listView}> Movie List</div>
       </main>
     </div>
