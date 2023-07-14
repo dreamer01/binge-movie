8d906dec7965601e25435e9b3693b71f2e4d3a76
Pushpendra Singh <psnitsri01@gmail.com>
2023-07-15 00:15:14 +0530

    Adding MovieCard and rendering list

---
+++ src/components/MovieCard/index.jsx
@@ -0,0 +1,25 @@
+import PropTypes from 'prop-types';
+
+import { Add, Play } from '../Icons';
+import Styles from './moviecard.module.css';
+
+const MovieCard = ({ title, poster, onClick }) => {
+  return (
+    <button className={Styles.card} onClick={onClick}>
+      <img className={Styles.poster} alt={`${title}-poster`} src={poster} />
+      <p className={Styles.title}>{title}</p>
+      <div className={Styles.actionView}>
+        <Play className={Styles.actionIcon} />
+        <Add className={Styles.actionIcon} />
+      </div>
+    </button>
+  );
+};
+
+MovieCard.propTypes = {
+  title: PropTypes.string.isRequired,
+  poster: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired,
+};
+
+export default MovieCard;
--- src/pages/Discover/index.jsx
+++ src/pages/Discover/index.jsx
@@ -1,12 +1,23 @@
 import { useState } from 'react';
 
+import { MOVIES } from '../../constants/movies';
 import Sidebar from '../../components/Sidebar';
-import Styles from './discover.module.css';
 import Search from '../../components/SearchInput';
+import MovieCard from '../../components/MovieCard';
+import Styles from './discover.module.css';
 
 function Discover() {
   const [showMenu, toggleMenu] = useState(false);
   const [search, setSearch] = useState('');
+
+  const renderMovie = ({ title, poster }) => (
+    <MovieCard
+      key={title}
+      title={title}
+      poster={poster}
+      onClick={() => console.log(title)}
+    />
+  );
 
   return (
     <div className={Styles.wrapper}>
@@ -27,8 +38,7 @@ function Discover() {
             onChange={(event) => setSearch(event.target.value)}
           />
         </div>
-        {/* <button onClick={() => toggleMenu((v) => !v)}> Toggle Menu</button> */}
-        <div className={Styles.listView}> Movie List</div>
+        <div className={Styles.listView}>{MOVIES.map(renderMovie)}</div>
       </main>
     </div>
   );
